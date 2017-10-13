c32f2deb204244a63afd8e961d26e0db4ca34f30
ycywmj <1036700236@qq.com>
2017-10-13 12:50:21 +0800

    Working Web Databse update

--- WebUI/Web/js/main_page.js
+++ WebUI/Web/js/main_page.js
@@ -19,47 +19,28 @@ function initDatabase()
     var ln=document.forms["CreatePatient"]["ln"].value;
     var psw=document.forms["CreatePatient"]["psw"].value;
     var sup=0;
-    var db=null;
-
-    var insetString="INSERT INTO USER (UserID, Password, IsSupervisor, Firstname, LastName) VALUES ('123','123',0,'123','123')";
 
     if(document.forms["CreatePatient"]["supervisor"].checked)
     {
         var sup=document.forms["CreatePatient"]["supervisor"].value;
     }
 
-    var NewString=pid+","+fn+","+ln+","+psw+","+sup;
+    //var NewString=pid+","+fn+","+ln+","+psw+","+sup;
     //alert(insetString);
 
-    var myDB = new ACCESSdb("KinesisArcade.mdb", {showErrors:true});
-
-    var SQL = "INSERT INTO USER VALUES('123', 'abc 123', 0,'xyz','123')";
-    var resultSet = myDB.query(SQL);
-
-    if(myDB.query(SQL))
-    {
-        alert("Yes");
-    }
+    var FileN = "C:\\Study/Murdoch/2017_S2/ICT302/ICT302_MGU/WebUI/Web/js/KinesisArcade.mdb";
+    //src = scripts[scripts.length-1].src;
+    //alert(scripts);
+    //var myDB = new ACCESSdb("C:\\Study/Murdoch/2017_S2/ICT302/ICT302_MGU/WebUI/Web/js/KinesisArcade.mdb");
+    var myDB = new ACCESSdb(FileN);
+    var SQL = "SELECT FirstName FROM [USER] WHERE UserID='1';";
+    var rsXML = myDB.query(SQL, {xml:true});
+    var FN=rsXML.getElementsByTagName("FirstName")[0].childNodes[0].nodeValue;
+    alert(FN);
 
 }
 
 function CreateDatabase()
 {
-    //Create the database
-    var db = new SQL.Database();
-    // Run a query without reading the results
-    db.run("CREATE TABLE test (col1, col2);");
-    // Insert two rows: (1,111) and (2,222)
-    db.run("INSERT INTO test VALUES (?,?), (?,?)", [1,111,2,222]);
 
-    // Prepare a statement
-    var stmt = db.prepare("SELECT * FROM test WHERE col1 BETWEEN $start AND $end");
-    stmt.getAsObject({$start:1, $end:1}); // {col1:1, col2:111}
-
-    // Bind new values
-    stmt.bind({$start:1, $end:2});
-    while(stmt.step()) { //
-        var row = stmt.getAsObject();
-        // [...] do something with the row of result
-    }
 }
