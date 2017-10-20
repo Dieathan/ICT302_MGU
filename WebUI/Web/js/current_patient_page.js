00596f667f397db6276dcba7e928341a0fff6320
ycywmj <1036700236@qq.com>
2017-10-20 11:49:23 +0800

    Web UI update+Wall

--- WebUI/Web/js/current_patient_page.js
+++ WebUI/Web/js/current_patient_page.js
@@ -3,15 +3,16 @@
 
 function  CurrentProgram() {
     window.location.href = "CurrentProgram.html";
+
 }
 
 
 function CreateNewProgram() {
 
-        var SQL = "INSERT INTO [PROGRAM] ('UserID') VALUES('" + getCookie("pid") + "');";
+        var SQL = "INSERT INTO [PROGRAM] (UserID) VALUES('" + getCookie("pid") + "');";
         var rsXML = myDB.query(SQL, {xml: true});
 
-        if(rsXML)
+        if(!rsXML)
         {
             alert("New Program created!");
         }
