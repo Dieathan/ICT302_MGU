4bdd2040bffe01b23461fe2938b0b8bb99d79879
Mengjiao <1837582601@qq.com>
2017-11-05 16:45:09 +0800

    Revert "Revert "Merge branch 'master' of https://github.com/Dieathan/ICT302_MGU""
    This reverts commit 02b18f1f1525b2f4b8b9d8994f9be6146bbd4815.

--- WebUI/Web/js/current_patient_page.js
+++ WebUI/Web/js/current_patient_page.js
@@ -1,8 +1,6 @@
 
 function  CurrentProgram() {
     window.location.href = "CurrentProgram.html";
-<<<<<<< HEAD
-
 }
 
 
@@ -12,15 +10,40 @@ function CreateNewProgram() {
 
         var pn=document.forms["NewProgram"]["pn"].value;
 
+        var myDB = new ACCESSdb(FileN);
+        //alert("New Program created!");
+
         var SQL = "INSERT INTO [PROGRAM](UserID,ProgramName,Time) VALUES('" + getCookie("pid") + "','"+pn+"','"+n+"');";
         var rsXML = myDB.query(SQL, {xml: true});
 
-        if(!rsXML)
+        if(rsXML)
         {
             alert("New Program created!");
         }
 
+}
 
-=======
->>>>>>> 382cb223d0bca2f4c5846f7e46d1f7cf8c9790e4
+function CurrentFeedBacks() {
+    window.location.href = "Feedbacks.html";
+}
+
+function CreateRestriction() {
+
+    var re = 0;
+    var gid = document.forms["NewRestriction"]["gid"].value;
+
+    if (document.forms["NewRestriction"]["restriction"].checked) {
+        var re = document.forms["NewRestriction"]["restriction"].value;
+    }
+
+    var myDB = new ACCESSdb(FileN);
+    //alert("New Program created!");
+
+    var SQL = "INSERT INTO [RESTRICTION](UserID,GameID,IsRestricted) VALUES('" + getCookie("pid") + "'," + gid + "," + re + ");";
+    var rsXML = myDB.query(SQL, { xml: true });
+
+    if (!rsXML) {
+        alert("Set!");
+    }
+
 }
